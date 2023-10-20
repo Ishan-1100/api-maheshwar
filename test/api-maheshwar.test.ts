fc20d838f74e802f8716d46c25aca9fad36033f4
ISHAN Ishan <ishan.na@soprasteria.com>
2023-10-20 08:31:48 +0530

    Initial commit

---
+++ test/api-maheshwar.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApiMaheshwar from '../lib/api-maheshwar-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/api-maheshwar-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApiMaheshwar.ApiMaheshwarStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
