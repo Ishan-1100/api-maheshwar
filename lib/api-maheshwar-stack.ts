05e59e9f43ef6747ebb794c50981b07b8c98ecdd
ISHAN Ishan <ishan.na@soprasteria.com>
2023-10-22 00:23:37 +0530

    first commit

--- lib/api-maheshwar-stack.ts
+++ lib/api-maheshwar-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import * as apigateway from 'aws-cdk-lib/aws-apigatewayv2';
+import * as api from 'aws-cdk-lib/aws-apigateway';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+// import * as aws from 'aws-cdk-lib/aws';
 
-export class ApiMaheshwarStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+class ApiMaheshwarStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiMaheshwarQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // Import the existing API Gateway
+    // const existingApi = apigateway..fromHttpApiAttributes(this, 'ExistingApi', {
+    //   httpApiId: 'YOUR_API_ID', // Replace with your existing API ID
     // });
+
+    // Import the existing API Gateway
+    const existingApi = api.RestApi.fromRestApiAttributes(this, 'ExistingApi', {
+      restApiId: 'YOUR_API_ID', // Replace with your existing API Gateway ID
+      rootResourceId: 'YOUR_ROOT_RESOURCE_ID', // Replace with the root resource ID
+    });
+    // Create CloudWatch metric for the existing API
+    const apiMetric = new cloudwatch.Metric({
+      namespace: 'AWS/ApiGateway',
+      metricName: 'Count',
+      dimensionsMap: {
+        ApiName: existingApi.restApiId,
+      },
+    });
+
+    // Create an Alarm for the metric
+    const alarm = new cloudwatch.Alarm(this, 'ExistingApiAlarm', {
+      metric: apiMetric,
+      threshold: 1, // Set your desired threshold
+      evaluationPeriods: 1,
+    });
   }
 }
+
+
+
+
